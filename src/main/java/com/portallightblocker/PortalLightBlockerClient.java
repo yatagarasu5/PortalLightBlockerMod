f9b2114bd05729a1aa0fa146b207afd2ea08a690
Renoceros <morenokristophan@gmail.com>
2025-10-31 22:34:53 +0700

    entrypoint Client

---
+++ src/main/java/com/portallightblocker/PortalLightBlockerClient.java
@@ -0,0 +1,11 @@
+package com.portallightblocker;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PortalLightBlockerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient(){
+
+	}
+	
+}
