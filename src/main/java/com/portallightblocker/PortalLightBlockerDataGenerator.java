70bd406f24cabd7bfacd1e2cc8ac2eeb4ba2bef7
Renoceros <morenokristophan@gmail.com>
2025-10-31 21:54:44 +0700

    ModTemplateInitStuffs

---
+++ src/main/java/com/portallightblocker/PortalLightBlockerDataGenerator.java
@@ -0,0 +1,11 @@
+package com.portallightblocker;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PortalLightBlockerDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
