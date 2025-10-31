fa904aa2c92af0e0af5051c44ea5a60fd709651e
Renoceros <morenokristophan@gmail.com>
2025-11-01 00:23:41 +0700

    Yeah sure buddy as if

--- src/main/java/com/portallightblocker/PortalLightBlocker.java
+++ src/main/java/com/portallightblocker/PortalLightBlocker.java
@@ -1,24 +1,59 @@
 package com.portallightblocker;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.block.NetherPortalBlock;
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.piston.PistonBehavior;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.registry.Registries;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 public class PortalLightBlocker implements ModInitializer {
-	public static final String MOD_ID = "portallightblocker";
+    @Override
+    public void onInitialize() {
+        System.out.println("[PortalLightBlocker] Replacing Nether Portal luminance...");
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+        // Create new Nether Portal block with light disabled
+        NetherPortalBlock newPortal = new NetherPortalBlock(
+            FabricBlockSettings.create()
+                .noCollision()
+                .ticksRandomly()
+                .strength(-1.0F)
+                .sounds(BlockSoundGroup.GLASS)
+                .luminance(state -> 0) // <-- light disabled
+                .pistonBehavior(PistonBehavior.BLOCK)
+        );
 
-	@Override
-	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+        try {
+            // 1️⃣ Replace the final static field in Blocks.NETHER_PORTAL
+            Field portalField = Blocks.class.getDeclaredField("NETHER_PORTAL");
+            portalField.setAccessible(true);
 
-		LOGGER.info("Hello Fabric world!");
-	}
+            // Remove 'final' modifier
+            Field modifiers = Field.class.getDeclaredField("modifiers");
+            modifiers.setAccessible(true);
+            modifiers.setInt(portalField, portalField.getModifiers() & ~Modifier.FINAL);
+
+            // Replace with our custom block
+            portalField.set(null, newPortal);
+
+            // 2️⃣ Replace the registry entry for nether_portal
+            var key = Identifier.ofVanilla("nether_portal");
+            ((net.minecraft.registry.MutableRegistry<NetherPortalBlock>) Registries.BLOCK).add(
+                net.minecraft.registry.RegistryKey.of(Registries.BLOCK.getKey(), key),
+                newPortal,
+                net.minecraft.registry.entry.RegistryEntryInfo.DEFAULT
+            );
+
+            System.out.println("[PortalLightBlocker] Nether Portal successfully overridden!");
+        } catch (Throwable e) {
+            System.err.println("[PortalLightBlocker] Failed to override Nether Portal block:");
+            e.printStackTrace();
+        }
+    }
 }
