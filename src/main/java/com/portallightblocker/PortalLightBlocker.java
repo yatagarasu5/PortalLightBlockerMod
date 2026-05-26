62d7246732ed656a2fdb504425687ea479a5e576
yatagarasu5 <kokorokatou05@gmail.com>
2026-05-26 10:44:06 +0900

    Update PortalLightBlocker.java

--- src/main/java/com/portallightblocker/PortalLightBlocker.java
+++ src/main/java/com/portallightblocker/PortalLightBlocker.java
@@ -3,10 +3,8 @@ package com.portallightblocker;
 import net.fabricmc.api.ModInitializer;
 
 public class PortalLightBlocker implements ModInitializer {
-
     @Override
     public void onInitialize() {
-        // Just confirm initialization
-        System.out.println("[PortalLightBlocker] Mod initialized. Nether Portal luminance patch active!");
+        System.out.println("[PortalLightBlocker] loaded");
     }
 }
