555cd370e6950c2084508cf5a04ffcc4315cb50d
yatagarasu5 <kokorokatou05@gmail.com>
2026-05-26 09:48:36 +0900

    Update BlocksMixin.java
    ｄｄｄｄ

--- src/main/java/com/portallightblocker/mixin/BlocksMixin.java
+++ src/main/java/com/portallightblocker/mixin/BlocksMixin.java
@@ -1,28 +1,19 @@
 package com.portallightblocker.mixin;
 
 import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.NetherPortalBlock;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyReturnValue;
 
-/**
- * Replace the luminance lambda used when registering the Nether Portal block.
- * Use the correct ordinal to target the luminance() invocation that applies to nether_portal.
- */
-@Mixin(Blocks.class)
-public class BlocksMixin {
+@Mixin(NetherPortalBlock.class)
+public class NetherPortalBlockMixin {
 
-    @ModifyArg(
-        method = "<clinit>",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/block/AbstractBlock$Settings;luminance(Ljava/util/function/ToIntFunction;)Lnet/minecraft/block/AbstractBlock$Settings;",
-            ordinal = 13 // <-- 13 instances of luminance()before NETHER_PORTAL in net.minecraft.block.Blocks
-        )
+    @ModifyReturnValue(
+        method = "getLuminance",
+        at = @At("RETURN")
     )
-    private static java.util.function.ToIntFunction<BlockState> portallightblocker$modifyNetherPortalLuminance(
-            java.util.function.ToIntFunction<BlockState> original) {
-        return state -> 0;
+    private int portallightblocker$noLight(int original, BlockState state) {
+        return 0;
     }
 }
